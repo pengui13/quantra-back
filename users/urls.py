287735ea6eb805044e19006c34859154c5e68b70
pengui13 <dansklyarov13@gmail.com>
2025-11-05 00:33:37 +0100

    upd urls + views

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView
+from .views import RegisterView, Ping
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("ping/", Ping.as_view(), name="ping"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from rest_framework import generics
 from .serializers import RegisterSerializer, EmailTokenObtainPairSerializer
+from rest_framework.views import APIView
 from drf_spectacular.utils import extend_schema
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
@@ -18,3 +22,9 @@ class RegisterView(generics.CreateAPIView):
 
 class EmailTokenObtainPairView(TokenObtainPairView):
     serializer_class = EmailTokenObtainPairSerializer
+
+class Ping(APIView):
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ("GET", "OPTIONS", "HEAD")
+    def get(self, request):
+        return Response({"message": "pong"}, status=200)
